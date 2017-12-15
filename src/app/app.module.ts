bc0a4d749fbc48f9b8300297d08321fded37be8a
Precious Jahlom Agboado <jahlom@preterk.com>
2017-12-15 09:32:46 +0000

    first release

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
-
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { MyApp } from './app.component';
 
 @NgModule({
--- src/pages/home/home.module.ts
+++ src/pages/home/home.module.ts
@@ -2,12 +2,19 @@ import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
 import { HomePage } from './home';
 
+import { MatButtonModule, MatCardModule, MatTabsModule, MatChipsModule, MatIconModule } from "@angular/material";
+
 @NgModule({
   declarations: [
     HomePage,
   ],
   imports: [
     IonicPageModule.forChild(HomePage),
+    MatButtonModule,
+    MatCardModule,
+    MatTabsModule,
+    MatChipsModule,
+    MatIconModule
   ],
   exports: [
     HomePage
